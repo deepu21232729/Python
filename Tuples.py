69cca49affd35bb8962fff021ba569916063b6b7
Deepika Piriya <99253037+deepu21232729@users.noreply.github.com>
2026-07-10 23:11:42 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,20 @@
+'''my_tuple = ("apple", 3, "cherry", "apple", 5.8)
+print(my_tuple)
+print(len(my_tuple))
+
+tup=("apple",)
+print(tup)
+print(type(my_tuple))
+
+my_tuple = ("apple", 3, "cherry", "apple", 5.8)     #accessing 
+print(my_tuple[0:3])
+print(my_tuple[:5])
+print(my_tuple[-4:-1])
+print(my_tuple[::-1])
+'''
+
+my_tuple = ("apple", 3, "cherry", "apple", 5.8)
+if 5.8 in my_tuple:
+    print("yes")
+else:
+    print("No")
